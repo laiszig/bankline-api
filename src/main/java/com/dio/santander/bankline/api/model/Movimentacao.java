12369822b16ba004e60f18aa5a43fdbee67b2f00
Laís Ziegler <79112829+laiszig@users.noreply.github.com>
2022-05-20 17:05:19 -0300

    create tables

---
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -0,0 +1,65 @@
+package com.dio.santander.bankline.api.model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tab_movimentacao")
+public class Movimentacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "data_hora")
+    private LocalDateTime dataHora;
+
+    private String descriao;
+    private Double valor;
+
+    @Enumerated(EnumType.STRING)
+    private MovimentacaoTipo tipo;
+
+    @Column(name = "id_conta")
+    private Integer idConta;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public String getDescriao() {
+        return descriao;
+    }
+
+    public void setDescriao(String descriao) {
+        this.descriao = descriao;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public MovimentacaoTipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(MovimentacaoTipo tipo) {
+        this.tipo = tipo;
+    }
+}
